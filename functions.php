00dc27bc1dd76825dbab2f83cfacb2970252e384
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-07-13 17:30:49 +0300

    add custom order status - delivered

---
+++ functions.php
@@ -0,0 +1,89 @@
+add_action('init', 'register_order_status');
+add_filter('wc_order_statuses', 'add_delivered_to_order_statuses');
+add_filter('woocommerce_reports_order_statuses', 'include_custom_order_status_to_reports', 20, 1);
+add_filter('woocommerce_order_is_paid_statuses', 'delivered_woocommerce_order_is_paid_statuses');
+add_filter('bulk_actions-edit-shop_order', 'add_bulk_actions', 50, 1 );
+
+/*** Register new status : Delivered
+ **/
+function register_order_status()
+{
+    register_post_status('wc-delivered', array(
+        'label' => __('Delivered', 'text-domain'),
+        'public' => true,
+        'show_in_admin_status_list' => true,
+        'show_in_admin_all_list' => true,
+        'exclude_from_search' => false,
+        'label_count' => _n_noop('Delivered <span class="count">(%s)</span>', 'Delivered <span class="count">(%s)</span>', 'text-domain')
+    ));
+}
+
+/*
+ * add status after completed
+ */
+function add_delivered_to_order_statuses($order_statuses)
+{
+    $new_order_statuses = array();
+    foreach ($order_statuses as $key => $status) {
+        $new_order_statuses[$key] = $status;
+        if ('wc-completed' === $key) {
+            $new_order_statuses['wc-delivered'] = __('Delivered', 'text-domain');
+        }
+    }
+    return $new_order_statuses;
+}
+/*
+ * Adding the custom order status to the default woocommerce order statuses
+ */
+function include_custom_order_status_to_reports($statuses)
+{
+    if ($statuses)
+        $statuses[] = 'delivered';
+    return $statuses;
+}
+/*
+ * mark status as a paid.
+ */
+function delivered_woocommerce_order_is_paid_statuses($statuses)
+{
+    $statuses[] = 'delivered';
+    return $statuses;
+}
+
+* add bulk action
+* Change order status to delivered
+*/
+public function add_bulk_actions( $bulk_actions )
+{
+	$lable = wc_get_order_status_name( 'delivered' );
+	$bulk_actions['mark_delivered'] = __( 'Change status to ' . $lable . '', 'trackship-for-woocommerce' );
+	return $bulk_actions;
+}
+
+/**
+ * Class My_Custom_Status_WC_Email
+ */
+class Delivered_WC_Email {
+
+	/**
+	 * Delivered_WC_Email constructor.
+	 */
+	public function __construct() {
+		// Filtering the emails and adding our own email.
+		add_filter( 'woocommerce_email_classes', array( $this, 'register_email' ), 90, 1 );
+		// Absolute path to the plugin folder.
+		define( 'DELIVERED_WC_EMAIL_PATH', get_stylesheet_directory() );
+	}
+
+	/**
+	 * @param array $emails
+	 *
+	 * @return array
+	 */
+	public function register_email( $emails ) {
+		require_once 'emails/class-wc-delivered-status-order.php';
+		$emails['WC_Delivered_status_Order'] = new WC_Delivered_status_Order();
+		return $emails;
+	}
+}
+new Delivered_WC_Email();
