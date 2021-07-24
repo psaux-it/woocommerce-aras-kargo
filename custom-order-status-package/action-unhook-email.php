f3a320bbb3e5125cdd7d9a3cdd119b5f29abfef2
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-07-24 19:25:32 +0300

    remove shebang

--- custom-order-status-package/action-unhook-email.php
+++ custom-order-status-package/action-unhook-email.php
@@ -1,4 +1,3 @@
-<?php
 add_action( 'woocommerce_email', 'unhook_those_pesky_emails' );
 function unhook_those_pesky_emails( $email_class ) {
         remove_action( 'woocommerce_order_status_completed_notification', array( $email_class->emails['WC_Email_Customer_Completed_Order'], 'trigger' ) );
