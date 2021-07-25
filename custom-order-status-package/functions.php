0afd5237d4bf81193a61784766540ca51623f61c
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-07-25 16:10:22 +0300

    use include istead of include_once

--- custom-order-status-package/functions.php
+++ custom-order-status-package/functions.php
@@ -1,4 +1,4 @@
 <?php
 // woocommerce-aras-cargo-integration
-//aras_woo_include_once( get_stylesheet_directory() .'/woocommerce/aras-woo-delivered.php');
+//aras_woo_include( get_stylesheet_directory() .'/woocommerce/aras-woo-delivered.php');
 // woocommerce-aras-cargo-integration
