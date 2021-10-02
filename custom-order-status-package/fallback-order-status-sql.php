a9667dd64327f3338c222973c7021c8e8f2d3747
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-10-02 14:22:09 +0300

    fix table name

--- custom-order-status-package/fallback-order-status-sql.php
+++ custom-order-status-package/fallback-order-status-sql.php
@@ -4,7 +4,7 @@
 add_action('init', 'process_query');
 	function process_query(){
 		global $wpdb;
-		$table_name = $wpdb->prefix . 'wp-posts';
+		$table_name = $wpdb->prefix . 'posts';
 		$wpdb->query(
 			$wpdb->prepare( "UPDATE $table_name SET post_status = 'wc-completed' WHERE post_type = 'shop_order' AND post_status = 'wc-delivered'" )
 		);
