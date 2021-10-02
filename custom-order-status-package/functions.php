9667038efae0195a0a9f8e6e83101508025ad0dd
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-10-02 10:35:11 +0300

    code optimizations

--- custom-order-status-package/functions.php
+++ custom-order-status-package/functions.php
@@ -1,8 +1,6 @@
 <?php
 
 /** * woocommerce-aras-cargo-integration */
-
 //aras_woo_include( get_stylesheet_directory() .'/woocommerce/aras-woo-delivered.php');
-//aras_woo_include_once( get_stylesheet_directory() .'/woocommerce/fallback-order-status-sql.php');
-
+//woo_aras_include_once( get_stylesheet_directory() .'/woocommerce/fallback-order-status-sql.php');
 /** * woocommerce-aras-cargo-integration */
