ca530f4b9fcf0bf8b8082e084a3e7e7bb3da98c9
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-10-02 17:44:22 +0300

    code optimizations

--- custom-order-status-package/fallback-order-status-sql.php
+++ custom-order-status-package/fallback-order-status-sql.php
@@ -1,11 +1,13 @@
 <?php
 
 /** * woocommerce-aras-cargo-integration */
+
 add_action('init', 'process_query');
-	function process_query(){
-		global $wpdb;
-		$table_name = $wpdb->prefix . 'posts';
-		$wpdb->query(
-			$wpdb->prepare( "UPDATE $table_name SET post_status = 'wc-completed' WHERE post_type = 'shop_order' AND post_status = 'wc-delivered'" )
-		);
-	}
+
+function process_query() {
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'posts';
+	$wpdb->query(
+		$wpdb->prepare( "UPDATE $table_name SET post_status = 'wc-completed' WHERE post_type = 'shop_order' AND post_status = 'wc-delivered'" )
+	);
+}
