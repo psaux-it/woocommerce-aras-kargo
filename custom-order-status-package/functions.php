5fad6be80d75497f40e83e3cff35929f5a159e88
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-10-02 04:37:40 +0300

    add uninstall sql query

--- custom-order-status-package/functions.php
+++ custom-order-status-package/functions.php
@@ -1,4 +1,8 @@
 <?php
-// woocommerce-aras-cargo-integration
+
+/** * woocommerce-aras-cargo-integration */
+
 //aras_woo_include( get_stylesheet_directory() .'/woocommerce/aras-woo-delivered.php');
-// woocommerce-aras-cargo-integration
+//aras_woo_include_once( get_stylesheet_directory() .'/woocommerce/fallback-order-status-sql.php');
+
+/** * woocommerce-aras-cargo-integration */
