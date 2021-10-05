b5de943abcb35757c966578db5bec387591592da
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-10-05 22:33:10 +0300

    add description

--- woo-aras-setup.sh
+++ woo-aras-setup.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+#
+# Copyright (C) 2021 Hasan CALISIR <hasan.calisir@psauxit.com>
+# Distributed under the GNU General Public License, version 2.0.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# What is doing this script exactly?
+# -This is a wrapper/helper script for woocommerce and ARAS cargo integration setup.
+# -This script prepares necessary environment for ARAS cargo integration.
 
 # Ugly die
 die () {
