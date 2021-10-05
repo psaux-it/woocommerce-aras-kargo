117b1b7a4913c40e71a0fee9b2d7cffe461834df
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-10-05 22:10:59 +0300

    code optimizations

--- woo-aras-setup.sh
+++ woo-aras-setup.sh
@@ -3,18 +3,32 @@
 # Global Variables
 # =====================================================================
 export new_user="wooaras"
-export setup_key=wooaras
+export setup_key="gajVVK2zXo"
 export installation_path="/home/${new_user}/scripts/woocommerce-aras-cargo"
-export password="Tech5self9pack"
-enc_pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 sudoers_file="/etc/sudoers"
-sudoers_d_file="/etc/sudoers.d/${new_user}"
 pass_file="/etc/passwd"
 
 # My style
 # =====================================================================
 green=$(tput setaf 2); red=$(tput setaf 1); reset=$(tput sgr0); cyan=$(tput setaf 6)
-magenta=$(tput setaf 5); yellow=$(tput setaf 3); m_tab='  '
+magenta=$(tput setaf 5); yellow=$(tput setaf 3); m_tab='  '; BC=$'\e[32m'; EC=$'\e[0m'
+
+# @EARLY CRITICAL CONTROLS --> means that no spend cpu time anymore
+# =====================================================================
+# Prevent errors cause by uncompleted upgrade
+# Detect to make sure the entire script is available, fail if the script is missing contents
+if [[ "$(tail -n 1 "${0}" | head -n 1 | cut -c 1-7)" != "exit \$?" ]]; then
+  echo -e "\n${red}*${reset} ${red}Script is incomplete, please force upgrade manually${reset}"
+  echo -e "${cyan}${m_tab}#####################################################${reset}\n"
+  exit 1
+fi
+
+# Check OS is supported
+if [[ "${OSTYPE}" != "linux-gnu"* ]]; then
+  echo -e "\n${red}*${reset} ${red}Unsupported operating system: $OSTYPE${reset}"
+  echo -e "${cyan}${m_tab}#####################################################${reset}\n"
+  exit 1
+fi
 
 # Determine Script path
 # =====================================================================
@@ -49,33 +63,37 @@ fi
 # Remove trailing / (removes / and //) from script path
 shopt -s extglob
 this_script_path="${this_script_path%%+(/)}"
+export temporary_path_x="${this_script_path}"
 
 # Create new group/user with home, grant sudo&cron privileges if not set
 # =====================================================================
 modified=0
+add_user () {
+  useradd -m -p "${enc_pass}" -s /bin/bash "${new_user}" >/dev/null 2>&1
+  usermod -a -G "${new_user}",cron"${1}" "${new_user}" >/dev/null 2>&1
+}
 if ! grep -qE "^${new_user}" "${pass_file}"; then
-  if grep -qE '%sudo.*ALL' "${sudoers_file}"; then
-    if ! grep -q "#" < <(grep -E '%sudo.*ALL' "${sudoers_file}"); then
-       useradd -m -p "${enc_pass}" -s /bin/bash "${new_user}" >/dev/null 2>&1
-       usermod -a -G "${new_user}",cron,sudo "${new_user}" >/dev/null 2>&1
-       modified=1
-    fi
+  echo -e "\n${green}*${reset} ${magenta}Setting 'wooaras' user password, type q for quit${reset}"
+  echo "${cyan}${m_tab}#####################################################${reset}"
+  read -r -p "${m_tab}${BC}Enter new 'wooaras' system user password:${EC} " password < /dev/tty
+  if [[ "${password}" == "q" || "${password}" == "quit" ]]; then exit 1; fi
+  echo "${cyan}${m_tab}#####################################################${reset}"
+  enc_pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+  if grep -qFx "%sudo ALL=(ALL) ALL" "${sudoers_file}" ||
+     grep -qFx '%sudo ALL=(ALL:ALL) ALL' "${sudoers_file}"; then
+     add_user ",sudo"
+     modified=1
   fi
-  if grep -qE '%sudo.*ALL' "${sudoers_file}"; then
-    if ! grep -q "#" < <(grep -E '%wheel.*ALL' "${sudoers_file}" | grep -v "NOPASSWD"); then
-      useradd -m -p "${enc_pass}" -s /bin/bash "${new_user}" >/dev/null 2>&1
-      usermod -a -G "${new_user}",cron,wheel "${new_user}" >/dev/null 2>&1
-      modified=1
-    fi
+  if grep -qFx '%wheel ALL=(ALL) ALL' "${sudoers_file}" ||
+     grep -qFx '%wheel ALL=(ALL:ALL) ALL' "${sudoers_file}"; then
+    add_user ",wheel"
+    modified=1
   fi
   if [[ "${modified}" -eq 0 ]]; then
-    useradd -m -p "${enc_pass}" -s /bin/bash "${new_user}" >/dev/null 2>&1
-    usermod -a -G "${new_user}",cron "${new_user}" >/dev/null 2>&1
-    if ! [[ -d "${sudoers_d_file%/*}" ]]; then
-      mkdir "${sudoers_d_file%/*}"
-      if ! [[ -e "${sudoers_d_file}" ]]; then
-        echo -e "${new_user}\tALL=(ALL:ALL) ALL" > "${sudoers_d_file}"
-      fi
+    add_user
+    if ! grep -qFx 'wooaras ALL=(ALL:ALL) ALL' "${sudoers_file}" &&
+       ! grep -qFx 'wooaras ALL=(ALL) ALL' "${sudoers_file}"; then
+      echo 'wooaras ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
     fi
   fi
 fi
@@ -86,7 +104,7 @@ fi
 mkdir -p "${installation_path}"
 
 # Copy files from temporary path to installation path & change ownership
-if ! [[ "$(ls -A ${installation_path})" ]]; then
+if ! [[ "$(ls -A ${installation_path} 2>/dev/null)" ]]; then
   cp -rT "${this_script_path}" "${installation_path}"
   chmod +x "${installation_path}"/woo-aras-setup.sh
   chmod +x "${installation_path}"/woocommerce-aras-cargo.sh
@@ -104,9 +122,9 @@ fi
 # Finally start the setup
 # =====================================================================
 if [[ "${1}" == "--force" || "${1}" == "-f" ]]; then
-  su -s /bin/bash -c 'sudo --preserve-env=new_user,setup_key,installation_path,password -S <<< '"${password}"' ./woocommerce-aras-cargo.sh --setup' "${new_user}"
+  su -s /bin/bash -c 'sudo --preserve-env=new_user,setup_key,installation_path,password,temporary_path_x -S <<< '"${password}"' ./woocommerce-aras-cargo.sh --setup' "${new_user}"
 elif ! [[ -f "${this_script_path}/.two.way.set" ]]; then
-  su -s /bin/bash -c 'sudo --preserve-env=new_user,setup_key,installation_path,password -S <<< '"${password}"' ./woocommerce-aras-cargo.sh --setup' "${new_user}"
+  su -s /bin/bash -c 'sudo --preserve-env=new_user,setup_key,installation_path,password,temporary_path_x -S <<< '"${password}"' ./woocommerce-aras-cargo.sh --setup' "${new_user}"
 else
   echo -e "\n${yellow}*${reset} ${green}Setup already completed.${reset}"
   echo "${cyan}${m_tab}#####################################################${reset}"
@@ -114,3 +132,6 @@ else
   echo -e "${m_tab}${magenta}sudo ./woo-aras-setup.sh --force${reset}\n"
   exit 1
 fi
+
+# And lastly we exit
+exit $?
