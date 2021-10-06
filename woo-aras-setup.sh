c9a546ee8306e29b6f4146259707125c32dde945
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-10-06 15:36:12 +0300

    code opt

--- woo-aras-setup.sh
+++ woo-aras-setup.sh
@@ -20,20 +20,6 @@
 # -This is a wrapper/helper script for woocommerce and ARAS cargo integration setup.
 # -This script prepares necessary environment for ARAS cargo integration.
 
-# Ugly die
-die () {
-  echo "$@" >&2
-  exit 1
-}
-
-# Global Variables
-# =====================================================================
-export new_user="wooaras"
-export setup_key="gajVVK2zXo"
-export installation_path="/home/${new_user}/scripts/woocommerce-aras-cargo"
-sudoers_file="/etc/sudoers"
-pass_file="/etc/passwd"
-
 # My style
 # =====================================================================
 green=$(tput setaf 2); red=$(tput setaf 1); reset=$(tput sgr0); cyan=$(tput setaf 6)
@@ -55,6 +41,38 @@ if [[ "${OSTYPE}" != "linux-gnu"* ]]; then
   echo -e "${cyan}${m_tab}#####################################################${reset}\n"
   exit 1
 fi
+
+usage () {
+  echo -e "\n${red}*${reset} ${red}Try to run script with root or sudo privileged user.${reset}"
+  echo "${cyan}${m_tab}#####################################################${reset}"
+  echo -e "${m_tab}${magenta}sudo ./woo-aras-setup.sh${reset}\n"
+}
+
+# Display usage for necessary privileges
+[[ ! $SUDO_USER && $EUID -ne 0 ]] && { usage; exit 1; }
+
+# Check git installed
+if ! command -v git > /dev/null 2>&1; then
+  echo -e "\n${yellow}*${reset} {yellow}git not found!${reset}"
+  echo "${cyan}${m_tab}#####################################################${reset}"
+  echo -e "${yellow}${m_tab}Install necessary package and re-start setup.${reset}\n"
+  exit 1
+fi
+
+# Ugly die
+die () {
+  echo "$@" >&2
+  exit 1
+}
+
+# Global Variables
+# =====================================================================
+export new_user="wooaras"
+export setup_key="gajVVK2zXo"
+export working_path="/home/${new_user}/scripts/woocommerce-aras-cargo"
+git_repo="https://github.com/hsntgm/woocommerce-aras-kargo.git"
+sudoers_file="/etc/sudoers"
+pass_file="/etc/passwd"
 
 # Determine Script path
 # =====================================================================
@@ -91,7 +109,7 @@ shopt -s extglob
 this_script_path="${this_script_path%%+(/)}"
 export temporary_path_x="${this_script_path}"
 
-# Create new group/user with home, grant sudo&cron privileges if not set
+# Create new user with home, grant sudo&cron privileges
 # =====================================================================
 modified=0
 
@@ -101,29 +119,31 @@ add_user () {
   usermod -a -G "${new_user}",cron"${1}" "${new_user}" >/dev/null 2>&1 || die "Could not add ${new_user} to ${1}"
 }
 
-# Check new user exist
+# Check new user exist, if not create
 if ! grep -qE "^${new_user}" "${pass_file}"; then
   echo -e "\n${green}*${reset} ${magenta}Setting ${new_user} user password, type q for quit${reset}"
   echo "${cyan}${m_tab}#####################################################${reset}"
-  read -r -p "${m_tab}${BC}Enter new ${new_user} system user password:${EC} " password < /dev/tty
+  read -r -p "${m_tab}${BC}Enter new system user password:${EC} " password < /dev/tty
   if [[ "${password}" == "q" || "${password}" == "quit" ]]; then exit 1; fi
   echo "${cyan}${m_tab}#####################################################${reset}"
 
   # Auto pass creation only support encrypted
   enc_pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 
-  # Check sudo group enabled
+  # Check sudo group
   if grep -qFx "%sudo ALL=(ALL) ALL" < <(< "${sudoers_file}" sed 's/[[:space:]]\+/ /g' | sed 's/^[ \t]*//') ||
      grep -qFx "%sudo ALL=(ALL:ALL) ALL" < <(< "${sudoers_file}" sed 's/[[:space:]]\+/ /g' | sed 's/^[ \t]*//'); then
      add_user ",sudo"
      modified=1
   fi
 
-  # Check wheel group enabled
+  # Check wheel group
   if grep -qFx "%wheel ALL=(ALL) ALL" < <(< "${sudoers_file}" sed 's/[[:space:]]\+/ /g' | sed 's/^[ \t]*//') ||
      grep -qFx "%wheel ALL=(ALL:ALL) ALL" < <(< "${sudoers_file}" sed 's/[[:space:]]\+/ /g' | sed 's/^[ \t]*//'); then
-     add_user ",wheel"
-     modified=1
+     if [[ "${modified}" -eq 0 ]]; then
+       add_user ",wheel"
+       modified=1
+     fi
   fi
 
   # Manually grant sudo privileges
@@ -136,27 +156,25 @@ if ! grep -qE "^${new_user}" "${pass_file}"; then
   fi
 fi
 
-# Prepeare the installation environment, if not set
+# Prepeare the installation environment for the first time
 # =====================================================================
 # Create installation path
-if [[ ! -d "${installation_path}" ]]; then
-  mkdir -p "${installation_path}" || die "Could not create directory ${installation_path}"
+if [[ ! -d "${working_path}" ]]; then
+  mkdir -p "${working_path}" || die "Could not create directory ${working_path}"
 fi
 
-# Copy files from temporary path to installation path & change ownership
-if ! [[ "$(ls -A ${installation_path})" ]]; then
-  cp -rT "${this_script_path}" "${installation_path}" || die "Could not copy to ${installation_path}"
-  chmod +x "${installation_path}"/woo-aras-setup.sh || die "Could not change mod woo-aras-setup.sh"
-  chmod +x "${installation_path}"/woocommerce-aras-cargo.sh || die "Could not change mod woocommerce-aras-cargo.sh"
-  if [[ "$(stat --format "%U" "${installation_path}/woo-aras-setup.sh" 2>/dev/null)" != "${new_user}" ]]; then
-    # Change ownership of installation path&files
-    chown -R "${new_user}":"${new_user}" "${installation_path%/*}" || die "Could not change ownership of ${installation_path%/*}"
-  fi
+# Clone repo to working path & change ownership
+if ! [[ "$(ls -A ${working_path})" ]]; then
+  cd "${working_path%/*}" || die "Could not change directory to ${working_path%/*}"
+  git clone --quiet "${git_repo}" . || die "Could not git clone"
+  chmod +x "${working_path}"/woo-aras-setup.sh || die "Could not change mod woo-aras-setup.sh"
+  chmod +x "${working_path}"/woocommerce-aras-cargo.sh || die "Could not change mod woocommerce-aras-cargo.sh"
+  chown -R "${new_user}":"${new_user}" "${working_path%/*}" || die "Could not change ownership of ${working_path%/*}"
 fi
 
-# Change directory to installation path
-if [[ "$(pwd)" != "${installation_path}" ]]; then
-  cd "${installation_path}" || die "Could not change directory to ${installation_path}"
+# Change directory to working path
+if [[ "$(pwd)" != "${working_path}" ]]; then
+  cd "${working_path}" || die "Could not change directory to ${working_path}"
 fi
 
 setup_info () {
@@ -171,16 +189,16 @@ setup_info () {
 # =====================================================================
 if [[ "$(whoami)" != "${new_user}" ]]; then
   if [[ "${1}" == "--force" || "${1}" == "-f" ]]; then
-    su -s /bin/bash -c 'sudo --preserve-env=new_user,setup_key,installation_path,password,temporary_path_x -S <<< '"${password}"' ./woocommerce-aras-cargo.sh --setup' "${new_user}"
+    su -s /bin/bash -c 'sudo --preserve-env=new_user,setup_key,working_path,password,temporary_path_x -S <<< '"${password}"' ./woocommerce-aras-cargo.sh --setup' "${new_user}"
   elif ! [[ -f "${this_script_path}/.two.way.set" ]]; then
-    su -s /bin/bash -c 'sudo --preserve-env=new_user,setup_key,installation_path,password,temporary_path_x -S <<< '"${password}"' ./woocommerce-aras-cargo.sh --setup' "${new_user}"
+    su -s /bin/bash -c 'sudo --preserve-env=new_user,setup_key,working_path,password,temporary_path_x -S <<< '"${password}"' ./woocommerce-aras-cargo.sh --setup' "${new_user}"
   else
     setup_info
   fi
 elif [[ "${1}" == "--force" || "${1}" == "-f" ]]; then
-  sudo ./woocommerce-aras-cargo.sh --setup
+  sudo --preserve-env=new_user,setup_key,working_path,password,temporary_path_x ./woocommerce-aras-cargo.sh --setup
 elif ! [[ -f "${this_script_path}/.two.way.set" ]]; then
-  sudo ./woocommerce-aras-cargo.sh --setup
+  sudo --preserve-env=new_user,setup_key,working_path,password,temporary_path_x ./woocommerce-aras-cargo.sh --setup
 else
   setup_info
 fi
