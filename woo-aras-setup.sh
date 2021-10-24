7803956722580b3f9c42b024646491aa3730ee09
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2021-10-24 11:09:55 +0300

    code opt

--- woo-aras-setup.sh
+++ woo-aras-setup.sh
@@ -119,9 +119,9 @@ fatal () {
 }
 
 # We have valid username?
-while [[ ! "${new_user}" =~ "^[a-z][-a-z0-9]*\$" ]]; do
-	fatal "Invalid username"
-done
+#while [[ ! "${new_user}" =~ "^[a-z][-a-z0-9]*\$" ]]; do
+#	fatal "Invalid username"
+#done
 
 done_ () {
   printf >&2 "\n${m_tab}${TPUT_BGGREEN}${TPUT_WHITE}${TPUT_BOLD} DONE ${TPUT_RESET} ${*}\n"
